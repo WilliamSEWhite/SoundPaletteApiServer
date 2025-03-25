74ebf6c0d14ec8a2c909687645fe3c282629a8f1
Victoria Danh <80844920+vd19qe@users.noreply.github.com>
2025-03-25 11:39:24 -0400

    Updated the LoadDatabase.sql script to include Following, Saved Post, Liked Posts, Messages etc.

--- SoundPaletteApiServer/Data/LoadDatabase.sql
+++ SoundPaletteApiServer/Data/LoadDatabase.sql
@@ -11,35 +11,38 @@ DBCC CHECKIDENT ('dbo.tUsers', RESEED, 0);
 USE [SP];
 GO
 
+/* ----- STATIC TABLES -----*/
 SELECT * FROM dbo.tPostTypes;
 SELECT * FROM dbo.tLocations;
+SELECT * FROM dbo.tTags;
 
 
 /* ----- USERS ----- */
 SELECT * FROM dbo.tUsers;
-INSERT INTO dbo.tUsers (Username, Password) VALUES
+INSERT INTO dbo.tUsers (Username, Password) 
+VALUES
     ('user1', 'pass1!'),
     ('user2', 'pass2!'),
     ('user3', 'pass3!'),
     ('user4', 'pass4!');
 SELECT * FROM dbo.tUsers;
 
-
 /* ----- USER INFO ----- */
 SELECT * FROM dbo.tUserInfos;
-INSERT INTO dbo.tUserInfos (userId, locationId, email, phone, dOB, dateCreated) VALUES
-   (1, 1, 'user1@example.com', '123-456-7890', '1995-05-10', GETUTCDATE()),
-   (2, 1, 'user2@example.com', '234-567-8901', '1992-08-22', GETUTCDATE()),
-   (3, 1, 'user3@example.com', '345-678-9012', '2000-02-15', GETUTCDATE()),
-   (4, 1, 'user4@example.com', '456-789-0123', '1988-11-30', GETUTCDATE());
+INSERT INTO dbo.tUserInfos (userId, locationId, email, phone, dOB, dateCreated) 
+VALUES
+    (1, 1, 'user1@example.com', '123-456-7890', '1995-05-10', GETUTCDATE()),
+    (2, 1, 'user2@example.com', '234-567-8901', '1992-08-22', GETUTCDATE()),
+    (3, 1, 'user3@example.com', '345-678-9012', '2000-02-15', GETUTCDATE()),
+    (4, 1, 'user4@example.com', '456-789-0123', '1988-11-30', GETUTCDATE());
 SELECT * FROM dbo.tUserInfos;
-
 
 /* ----- USER PROFILE ----- */
 -- DELETE FROM dbo.tUserProfile;
 -- DBCC CHECKIDENT ('dbo.tUserProfile', RESEED, 0);
 SELECT * FROM dbo.tUserProfiles;
-INSERT INTO dbo.tUserProfiles (userId, bio, picture) VALUES
+INSERT INTO dbo.tUserProfiles (userId, bio, picture) 
+VALUES
     (1, 'Painter | Digital Artist | Creator. Capturing moments through color & texture. Commissions open','/my/profilePic.jpg'),
     (2, 'Words are my canvas. Sharing stories, thoughts, and raw emotions','/my/profilePic.jpg'),
     (3, 'Beatmaker | Producer | Mixer. Turning ideas into tracks. Always down for collabs – hit me up!','/my/profilePic.jpg'),
@@ -47,25 +50,36 @@ INSERT INTO dbo.tUserProfiles (userId, bio, picture) VALUES
 SELECT * FROM dbo.tUserProfiles;
 
 
-/* ----- TAGS ----- */
-SELECT * FROM dbo.tTags;
-
 /* ----- USER TAGS ----- */
 SELECT * FROM dbo.tUserTags;
-INSERT INTO dbo.tUserTags (userId, tagId) VALUES
-  (1, 1),
-  (1, 2),
-  (1, 6),
-  (1, 7),
-  (2, 3),
-  (2, 2),
-  (3, 2),
-  (3, 4),
-  (3, 5),
-  (4, 6),
-  (4, 7);
+INSERT INTO dbo.tUserTags (userId, tagId) 
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 6),
+    (1, 7),
+    (2, 3),
+    (2, 2),
+    (3, 2),
+    (3, 4),
+    (3, 5),
+    (4, 6),
+    (4, 7);
 SELECT * FROM dbo.tUserTags;
 
+
+/* ----- USER FOLLOWERS -----*/
+SELECT * FROM dbo.tUserFollowers;
+INSERT INTO dbo.tUserFollowers (FollowerId, FollowingId) 
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (2, 1),
+    (2, 3),
+    (3, 4),
+    (4, 3);
+SELECT * FROM dbo.tUserFollowers;
 
 /* ----- POSTS ----- */
 -- DELETE FROM dbo.tPostLikes;
@@ -73,27 +87,28 @@ SELECT * FROM dbo.tUserTags;
 -- DELETE FROM dbo.tPosts;
 -- DBCC CHECKIDENT ('dbo.tPosts', RESEED, 0);
 SELECT * FROM dbo.tPosts;
-INSERT INTO dbo.tPosts (userId, postTypeId, caption, isPremium, isMature, isDeleted, createdDate, publishDate, CommentCount, LikeCount) VALUES
-    (1, 1, 'Exploring new sounds!', 0, 0, 0, CAST(GETUTCDATE() AS DATE), CAST(GETUTCDATE() AS DATE), 3, 2),
-    (2, 3, 'new lyrics in the works! just need some good vocals...', 1, 0, 0, GETUTCDATE(), GETUTCDATE(), 9, 123),
-    (4, 1, 'Finished my latest track!', 0, 0, 0, GETUTCDATE(), GETUTCDATE(), 1, 33),
-    (3, 3, 'What my Friday nights are looking like..', 0, 0, 0, GETUTCDATE(), GETUTCDATE(), 10,3),
-    (1, 1, 'Trying out a new genre today!', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),3,409),
-    (2, 3, 'they call this position the birds eye view I believe', 1, 0, 0, GETUTCDATE(), GETUTCDATE(),4,32),
-    (2, 1, 'Inspired by nature 🌿', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),12,39),
-    (4, 3, 'Sunday Funday!', 0, 1, 0, GETUTCDATE(), GETUTCDATE(),2,3049),
-    (4, 1, 'Here’s a snippet from my next album.', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),4,54),
-    (3, 3, 'just my morning commute guys…', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),9,43),
-    (2, 1, 'Collab opportunity for vocalists!', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),4,35),
-    (1,3, 'inspiration.', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),2,49),
-    (2, 1, 'A little jazz influence in this one.', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),9,384),
-    (3, 3, 'writing. dreaming. thinking.', 1, 0, 0, GETUTCDATE(), GETUTCDATE(),8,941),
-    (4, 1, 'Live session from last night!', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),10,8),
-    (2, 3, 'yes mom, this is work.', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),5,938),
-    (3, 1, 'Chill beats for focus.', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),9,43),
-    (1, 3, 'Art class schedule will be release on Sunday! Thank you for your patiences', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),2,489),
-    (2, 1, 'Throwback to an old favorite of mine.', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),4,98),
-    (4, 3, 'I cant wait to share this melody with you guys! Just needs some lyrics… HMU!', 0, 0, 0, GETUTCDATE(), GETUTCDATE(),3,49);
+INSERT INTO dbo.tPosts (userId, postTypeId, caption, isPremium, isMature, isDeleted, createdDate, publishDate) 
+VALUES
+    (1, 1, 'Exploring new sounds!', 0, 0, 0, CAST(GETUTCDATE() AS DATE), CAST(GETUTCDATE() AS DATE)),
+    (2, 3, 'new lyrics in the works! just need some good vocals...', 1, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (4, 1, 'Finished my latest track!', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (3, 3, 'What my Friday nights are looking like..', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (1, 1, 'Trying out a new genre today!', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (2, 3, 'they call this position the birds eye view I believe', 1, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (2, 1, 'Inspired by nature 🌿', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (4, 3, 'Sunday Funday!', 0, 1, 0, GETUTCDATE(), GETUTCDATE()),
+    (4, 1, 'Here’s a snippet from my next album.', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (3, 3, 'just my morning commute guys…', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (2, 1, 'Collab opportunity for vocalists!', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (1,3, 'inspiration.', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (2, 1, 'A little jazz influence in this one.', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (3, 3, 'writing. dreaming. thinking.', 1, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (4, 1, 'Live session from last night!', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (2, 3, 'yes mom, this is work.', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (3, 1, 'Chill beats for focus.', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (1, 3, 'Art class schedule will be release on Sunday! Thank you for your patiences', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+    (2, 1, 'Throwback to an old favorite of mine.', 0, 0, 0, GETUTCDATE(), GETUTCDATE()),
+(4, 3, 'I cant wait to share this melody with you guys! Just needs some lyrics… HMU!', 0, 0, 0, GETUTCDATE(), GETUTCDATE());
 SELECT * FROM dbo.tPosts;
 
 
@@ -101,35 +116,189 @@ SELECT * FROM dbo.tPosts;
 -- DELETE FROM dbo.tPostContents;
 -- DBCC CHECKIDENT ('dbo.tPostContents', RESEED, 0);
 SELECT * FROM dbo.tPostContents;
-INSERT INTO dbo.tPostContents (postId, postTextContent, s3Token) VALUES
-     (1, 'Sharing some thoughts on my latest sound exploration.', NULL),
-     (2, 'image02.png',NULL),
-     (3, 'Excited to finally share this track with everyone!', NULL),
-     (4, 'image07.png', NULL),
-     (5, 'Trying out something new and unexpected today!', NULL),
-     (6, 'image09.png', NULL),
-     (7, 'Nature always inspires my melodies.', NULL),
-     (8, 'image14.png', NULL),
-     (9, 'A sneak peek at my upcoming album. Hope you like it!', NULL),
-     (10, 'image18.png', NULL),
-     (11, 'Looking for a vocalist to collaborate with on my next track.', NULL),
-     (12, 'image21.png', NULL),
-     (13, 'Blending jazz influences into my latest piece.', NULL),
-     (14, 'image22.png', NULL),
-     (15, 'Captured a raw, live session last night. Pure energy!', NULL),
-     (16, 'image25.png', NULL),
-     (17, 'Lo-fi beats to help you focus and relax.', NULL),
-     (18, 'image29.png', NULL),
-     (19, 'Throwing it back to one of my earliest creations.', NULL),
-     (20, 'image10.png', NULL);
+INSERT INTO dbo.tPostContents (postId, postTextContent, s3Token) 
+VALUES
+    (1, 'Sharing some thoughts on my latest sound exploration.', NULL),
+    (2, 'image02.png',NULL),
+    (3, 'Excited to finally share this track with everyone!', NULL),
+    (4, 'image07.png', NULL),
+    (5, 'Trying out something new and unexpected today!', NULL),
+    (6, 'image09.png', NULL),
+    (7, 'Nature always inspires my melodies.', NULL),
+    (8, 'image14.png', NULL),
+    (9, 'A sneak peek at my upcoming album. Hope you like it!', NULL),
+    (10, 'image18.png', NULL),
+    (11, 'Looking for a vocalist to collaborate with on my next track.', NULL),
+    (12, 'image21.png', NULL),
+    (13, 'Blending jazz influences into my latest piece.', NULL),
+    (14, 'image22.png', NULL),
+    (15, 'Captured a raw, live session last night. Pure energy!', NULL),
+    (16, 'image25.png', NULL),
+    (17, 'Lo-fi beats to help you focus and relax.', NULL),
+    (18, 'image29.png', NULL),
+    (19, 'Throwing it back to one of my earliest creations.', NULL),
+    (20, 'image10.png', NULL);
 SELECT * FROM dbo.tPostContents;
+
+
+
+/* ----- POST TAGS -----*/
+SELECT * FROM dbo.tPostTags;
+INSERT INTO dbo.tPostTags (PostId, TagId) 
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 7),
+    (2, 2),
+    (2, 4),
+    (3, 6),
+    (3, 7),
+    (4, 3),
+    (4, 4),
+    (4, 5),
+    (5, 3),
+    (5, 5),
+    (5, 6),
+    (5, 7),
+    (6, 2),
+    (6, 3),
+    (7, 4),
+    (7, 5),
+    (7, 6),
+    (7, 7),
+    (8, 1),
+    (8, 5),
+    (9, 1),
+    (9, 3),
+    (10, 1),
+    (10, 2),
+    (10, 3),
+    (11, 2),
+    (11, 4),
+    (11, 5),
+    (11, 6),
+    (11, 7),
+    (12, 1),
+    (12, 3),
+    (13, 4),
+    (13, 5),
+    (14, 1),
+    (14, 3),
+    (14, 5),
+    (15, 2),
+    (15, 6),
+    (16, 6),
+    (16, 7),
+    (16, 4),
+    (17, 2),
+    (17, 5),
+    (18, 1),
+    (18, 3),
+    (18, 5),
+    (19, 3),
+    (19, 4),
+    (19, 5),
+    (19, 6),
+    (19, 7),
+    (20, 1),
+    (20, 4),
+    (20, 5);
+SELECT * FROM dbo.tPostTags;
+
+
+/* ----- POST LIKES -----*/
+SELECT * FROM dbo.tPostLikes;
+INSERT INTO dbo.tPostLikes (PostId, UserId) 
+VALUES
+    (1, 1),
+    (2, 1),
+    (3, 1),
+    (6, 1),
+    (10, 1),
+    (11, 1),
+    (14, 1),
+    (15, 1),
+    (17, 1),
+    (19, 1),
+    (1, 2),
+    (4, 2),
+    (5, 2),
+    (10, 2),
+    (11, 2),
+    (17, 2),
+    (18, 2),
+    (19, 2),
+    (20, 3),
+    (1, 3),
+    (2, 3),
+    (3, 3),
+    (4, 3),
+    (5, 3),
+    (6, 3),
+    (7, 3),
+    (8, 3),
+    (9, 3),
+    (10, 3),
+    (11, 3),
+    (12, 3),
+    (13, 3),
+    (14, 3),
+    (15, 3),
+    (16, 3),
+    (17, 3),
+    (18, 3),
+    (19, 3),
+    (20, 3),
+    (10, 4),
+    (13, 4),
+    (14, 4),
+    (15, 4);
+SELECT * FROM dbo.tPostLikes;
+
+/* ----- POST SAVED -----*/
+SELECT * FROM dbo.tPostSaves;
+INSERT INTO dbo.tPostSaves (PostId, UserId) 
+VALUES
+    (1, 1),
+    (4, 1),
+    (6, 1),
+    (9, 1),
+    (12, 1),
+    (15, 1),
+    (1, 2),
+    (3, 2),
+    (5, 2),
+    (7, 2),
+    (9, 2),
+    (11, 2),
+    (13, 2),
+    (15, 2),
+    (17, 2),
+    (19, 2),
+    (2, 3),
+    (4, 3),
+    (6, 3),
+    (8, 3),
+    (10, 3),
+    (12, 3),
+    (14, 3),
+    (16, 3),
+    (18, 3),
+    (20, 3),
+    (1, 4),
+    (3, 4),
+    (8, 4),
+    (5, 4);
+SELECT * FROM dbo.tPostSaves;
+
 
 
 /* ----- COMMENTS ----- */
 -- DELETE FROM dbo.tPostComments;
 -- DBCC CHECKIDENT ('dbo.tPostComments', RESEED, 0);
 SELECT * FROM dbo.tPostComments;
-INSERT INTO dbo.tPostComments (postId, userId, commentContent, createdDate) VALUES
+INSERT INTO dbo.tPostComments (postId, userId, commentContent, createdDate) 
+VALUES
     (1, 2, 'Love the vibe on this one!', GETUTCDATE()),
     (1, 3, 'Really unique sound. Keep it up!', GETUTCDATE()),
     (1, 4, 'Chill beats. Definitely adding this to my playlist.', GETUTCDATE()),
@@ -187,5 +356,47 @@ INSERT INTO dbo.tPostComments (postId, userId, commentContent, createdDate) VALU
     (10, 4, 'Post more like this.', GETUTCDATE()),
     (10, 2, 'This is amazing.', GETUTCDATE()),
     (10, 3, 'Vibe unmatched.', GETUTCDATE());
-
 SELECT * FROM dbo.tPostComments;
+
+
+
+/* ----- CHATROOMS ----- */
+SELECT * FROM dbo.tChatRooms;
+INSERT INTO dbo.tChatRooms (ChatRoomName, CreatedDate, IsGroupChat) 
+VALUES
+    ('Creative Vibes', GETUTCDATE(), 0),
+    ('User1 Album3', GETUTCDATE(), 1);
+SELECT * FROM dbo.tChatRooms;
+
+/* ----- CHATROOM MEMBERS ----- */
+SELECT * FROM dbo.tChatroomMembers;
+INSERT INTO dbo.tChatroomMembers (ChatroomId, UserId, IsActive) 
+VALUES
+    (1, 1, 1),
+    (1, 2, 1),
+    (2, 1, 1),
+    (2, 2, 1),
+    (2, 4, 1);
+SELECT * FROM dbo.tChatroomMembers;
+
+/* ----- CHATROOM MESSAGES ----- */
+SELECT * FROM dbo.tMessages;
+INSERT INTO dbo.tMessages (Message, SentDate, ChatroomMemberId, isDeleted) 
+VALUES
+    ('Hey team, the new track is almost ready!', GETUTCDATE(), 1, 0),
+    ('Can’t wait to hear it!', GETUTCDATE(),2, 0),
+    ('This is definitely going to be a #1 hit', GETUTCDATE(), 1, 0),
+    ('John said hes down to join the listening party', GETUTCDATE(),2, 0),
+    ('He wants to know if he should bring something???', GETUTCDATE(),2, 0),
+    
+    ('Okay so, how many tracks are we aiming for on the album?', GETUTCDATE(), 1, 0),
+    ('I think 10 is a solid number. Keeps it tight.', GETUTCDATE(), 2, 0),
+    ('Agreed. Quality over quantity.', GETUTCDATE(), 4, 0),
+    ('What about the genre mix? Are we sticking to R&B?', GETUTCDATE(), 1, 0),
+    ('Mostly R&B, but maybe experiment with some lo-fi vibes?', GETUTCDATE(), 2, 0),
+    ('Yes!! That would give the interludes a unique touch.', GETUTCDATE(), 4, 0),
+    ('Also, we need a title. Something that hits emotionally.', GETUTCDATE(), 1, 0),
+    ('How about *Midnight Letters*?', GETUTCDATE(), 4, 0),
+    ('Oooh that’s poetic. I love it.', GETUTCDATE(), 2, 0),
+    ('Alright, *Midnight Letters* it is! Let’s get to writing 🎶', GETUTCDATE(), 1, 0);
+SELECT * FROM dbo.tMessages;
